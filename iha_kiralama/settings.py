e4ebe95fb96b970592207d888114b95afe2ee7f0
Mustafa Simsek <mustafasimsekdev@gmail.com>
2024-10-28 15:20:09 +0300

    add config, base auth and change .env and settings.py

--- iha_kiralama/settings.py
+++ iha_kiralama/settings.py
@@ -10,23 +10,33 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from django.utils.translation import gettext_lazy as _
+from dotenv import load_dotenv
+
+from config.template import TEMPLATE_CONFIG, THEME_LAYOUT_DIR, THEME_VARIABLES
+
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w3#dnf0*%jm2yg-p&vvn-2#za1%7w3u!8^8x_w(%8^svdh_y*'
+SECRET_KEY = os.environ.get("SECRET_KEY", default='django-insecure-5w3#dnf0*%jm2yg-p&vvn-2#za1%7w3u!8^8x_w(%8^svdh_y*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 'True').lower() in ['true', 'yes', '1']
 
-ALLOWED_HOSTS = []
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+# Current DJANGO_ENVIRONMENT
+ENVIRONMENT = os.environ.get("DJANGO_ENVIRONMENT", default="local")
 
 # Application definition
 
@@ -41,7 +51,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,7 +66,7 @@ ROOT_URLCONF = 'iha_kiralama.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +74,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "config.context_processors.language_code",
+            ],
+            "libraries": {
+                "theme": "config.template_tags.theme",
+            },
+            "builtins": [
+                "django.templatetags.static",
+                "config.template_tags.theme",
             ],
         },
     },
@@ -69,17 +89,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iha_kiralama.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE", default=''),
+        'NAME': os.environ.get("DB_NAME", default=''),
+        'USER': os.environ.get("DB_USER", default=''),
+        'PASSWORD': os.environ.get("DB_PASSWORD", default=''),
+        'HOST': os.environ.get("DB_HOST", default=''),
+        'PORT': os.environ.get("DB_PORT", default=''),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -99,11 +121,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# Enable i18n and set the list of supported languages
+LANGUAGES = [
+    ("tr", _("Turkish")),
+    ("en", _("English")),
+    # Add more languages as needed
+]
+
+LANGUAGE_CODE = 'tr'
 
 TIME_ZONE = 'UTC'
 
@@ -111,13 +139,32 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "src/assets",
+]
+
+# Default URL on which Django application runs for specific environment
+BASE_URL = os.environ.get("BASE_URL", default="http://127.0.0.1:8000")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Template Settings
+# ------------------------------------------------------------------------------
+
+THEME_LAYOUT_DIR = THEME_LAYOUT_DIR
+TEMPLATE_CONFIG = TEMPLATE_CONFIG
+THEME_VARIABLES = THEME_VARIABLES
